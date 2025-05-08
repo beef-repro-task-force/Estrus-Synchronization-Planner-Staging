e6b451b3f593c937a5083f7687cc56b4a712d6c0
Cristian Miclea <>
2025-05-08 15:09:52 +0300

    Change route to staging

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./style/index.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/Estrus-Synchronization-Planner'>
+    <BrowserRouter basename='/Estrus-Synchronization-Planner-Staging'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
