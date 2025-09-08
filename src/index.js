3313eab14dd985a973062a80920f59cb76039d69
Cristian Miclea <>
2025-09-08 20:53:49 +0300

    Update start script

--- src/index.js
+++ src/index.js
@@ -6,13 +6,25 @@ import { BrowserRouter } from 'react-router-dom';
 
 import "./style/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const basename = window.BEEF_APP_BASENAME || "/Estrus-Synchronization-Planner-Staging";
+let basename = "/Estrus-Synchronization-Planner-Staging";
+if (typeof window.BEEF_APP_BASENAME === "string") {
+  basename = window.BEEF_APP_BASENAME;
+}
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+window.renderBeefApp = function() {
+  const rootElement = document.getElementById('root');
+  if (!rootElement) return;
+
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter basename={basename}>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+
+if (document.getElementById('root')) {
+  window.renderBeefApp();
+}
